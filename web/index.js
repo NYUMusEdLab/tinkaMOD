885d90ce9dd7d34f8906b063f77d07617f1ff4b2
Willie Payne <williepain@gmail.com>
2019-02-12 20:09:28 -0500

    Added tinkacore/tinkasensor web support. Tested disconnect and reconnect

--- web/index.js
+++ web/index.js
@@ -1,31 +1,99 @@
+let tinkaServiceName = 0xfffa;
+var slider = document.getElementById('slider');
+
+// Place to hold tinkacores and information about them
+let tinkacores = {}; // Dict containing all tinkacores
+TinkaCore.core_ids = TinkaCore.core_ids || {
+    connected: new Set([]),
+    disconnected: new Set([])
+}
 
 function connect(){
-    console.log('Requesting Bluetooth Device...')
+    console.log('Requesting Bluetooth Device...');
+    let newDeviceID; // Hold on to the device ID for later
     navigator.bluetooth.requestDevice({
 	filters : [{
-	    name: 'Tinka'
+	    name: 'Tinka',
 	}],
-	//acceptAllDevices: true ,
+	optionalServices: [tinkaServiceName]
     })
 	.then(device => {
 	    console.log('> Found ' + device.name);
 	    console.log('> Id: ' + device.id);
 	    console.log('> Connected: ' + device.gatt.connected);
 	    device.addEventListener('gattserverdisconnected', onDisconnected);
-	    return device.gatt.connect() //device.gatt.disconnect()
+
+        newDeviceID = device.id;
+        return device.gatt.connect()
 	})
 	.then(server => {
-	    console.log('Tinka services...');
-	    //https://www.bluetooth.com/specifications/gatt/characteristics
-	    return service.getPrimaryService();
+            console.log(server);
+	    return server.getPrimaryService(tinkaServiceName);
+	})
+	.then(service => {
+            console.log('Tinka services...');
+            console.log(service);
+            return service.getCharacteristics();
+	})
+	.then(characteristics => {
+	    console.log('Tinka characteristics...');
+        add_tinkacore(newDeviceID, characteristics);
 	})
 	.catch(error => {
 	    console.log("ERROR: " + error);
 	});
 }
 
+function add_tinkacore(id, characteristics) {
+    if (TinkaCore.core_ids.disconnected.has(id)) {
+        tinkacores[id].reconnect(characteristics);
+    }
+    else {
+        let newTinkaCore = new TinkaCore(id, characteristics);
+        newTinkaCore.connect();
+        tinkacores[id] = new TinkaCore(id, characteristics);
+    }
+
+    console.log(tinkacores);
+    console.log(TinkaCore.core_ids);
+    return tinkacores;
+}
+
+function handleChange(event) {
+    // Produces the same array as used in the Node code
+    let buff = new Uint8Array(event.target.value.buffer);
+    console.log(buff);
+}
 
 function onDisconnected(event) {
-  let device = event.target;
-  console.log('Device ' + device.name + ' is disconnected.');
+    let device = event.target;
+    let disconnected_id = device.id;
+
+    tinkacores[disconnected_id].disconnect();
+    console.log(TinkaCore.core_ids);
+    console.log('Device ' + device.name + ' is disconnected.');
 }
+
+
+// Old Motor Code
+//0-4
+//0-255
+
+/*
+function stop_motor(event) {
+    slider.value = 0
+    //doesn't work yet
+}
+*/
+
+/*slider.oninput = function() {
+var speed = parseInt(this.value, 10)
+if( speed < 0 ){
+    d = 0;
+    speed *= -1;
+}
+else{ d = 255; }
+var x = Math.floor(speed/255) //+1 <- never goes to 0
+var y = speed % 255
+var motor = new Uint8Array([90,171,10,0,0,2,5,0,0,d,x,y])
+characteristics[1].writeValue(motor);*/
---
+++ web/tinkacore.js
@@ -0,0 +1,137 @@
+class TinkaCore {
+
+    // Coming from the Static variable, this might also have a number
+    // for when it was added to the array
+    constructor(id, characteristics) {
+        // Instance Variables
+        this.characteristics = characteristics; // noble bluetooth component
+        this.id = id;
+        this.connected = true;
+        this.sensor_connected = false;
+        this.sensor = null;
+
+        // Static Variable
+        TinkaCore.core_ids = TinkaCore.core_ids || {
+            connected: new Set([]),
+            disconnected: new Set([])
+        }
+
+        TinkaCore.add_core(this.id);
+    }
+
+    connect() {
+        let self = this;
+        self.characteristics[0].startNotifications();
+
+        // If 'self' is not bound here. Then the event itself becomes 'self'
+        self.characteristics[0].addEventListener('characteristicvaluechanged',
+                    self.parse_packet.bind(self));
+
+        return true;
+    }
+
+    disconnect() {
+        this.connected = false;
+        TinkaCore.remove_core(this.id);
+        return false;
+    }
+
+    reconnect(characteristics) {
+        let self = this;
+
+        console.log('Reconnecting');
+        self.characteristics = characteristics;
+        TinkaCore.add_core(this.id);
+
+        self.connect();
+        return true;
+    }
+
+    connect_sensor(sensor_id) {
+        switch (sensor_id) {
+            case 1:
+                this.sensor = new Button();
+                break;
+            case 2:
+                this.sensor = new Knob();
+                break;
+            case 3:
+                this.sensor = new Slider();
+                break;
+            case 4:
+                this.sensor = new Joystick();
+                break;
+            case 23:
+                this.sensor = new Distance();
+                break;
+            case 27:
+                this.sensor = new Color();
+                break;
+            default:
+                this.sensor = new TinkaSensor();
+                console.log('not yet implemented');
+        }
+        this.sensor_connected = true;
+        console.log('Sensor connected: ', this.sensor.name);
+
+        return this.sensor;
+    }
+
+    disconnect_sensor() {
+        this.sensor_connected = false;
+        this.sensor = null;
+
+        console.log('Sensor disconnected');
+        return false;
+    }
+
+    parse_packet(event) {
+        let self = this;
+        let packet = new Uint8Array(event.target.value.buffer);
+
+        // Correctly formed packets should have at least length 10
+        if (packet.length < 10) {
+            console.log('Error: Invalid packet length; too short');
+            return false;
+        }
+
+        let packet_length = packet[2];
+        let sensor_id = packet[6];
+        let command_id = packet[8];
+        let command = packet.slice(9);
+
+        switch (sensor_id) {
+            case 0: // Connect/Disconnect
+                let new_sensor_id = command[0];
+                if (new_sensor_id == 255) { this.disconnect_sensor(); }
+                else { self.connect_sensor(new_sensor_id); }
+                break;
+            default:
+                if (!this.sensor_connected) { this.connect_sensor(sensor_id); }
+                if (sensor_id != this.sensor.id) { this.connect_sensor(sensor_id); }
+
+                else {
+                    let reading = this.sensor.sense(command_id, command);
+                    console.log(this.sensor.name + ': ', reading);
+                }
+        }
+    }
+
+    // Static Methods for keeping track globally of
+    // what cores we have connected
+    static add_core(peripheral_id) {
+        // Check if TinkaCore is undefined
+        if (TinkaCore.core_ids.disconnected.has(peripheral_id)) {
+            TinkaCore.core_ids.disconnected.delete(peripheral_id);
+        }
+        TinkaCore.core_ids.connected.add(peripheral_id);
+        return peripheral_id;
+    }
+
+    static remove_core(peripheral_id) {
+        TinkaCore.core_ids.connected.delete(peripheral_id);
+        TinkaCore.core_ids.disconnected.add(peripheral_id);
+        return peripheral_id;
+    }
+
+}
